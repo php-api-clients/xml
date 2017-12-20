99e0bc62468fdd5ba93ed267256cdbc149169392
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-12-20 17:29:49 +0100

    From and to XML constants

---
+++ tests/Constant.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tests\Middleware\Xml;
+
+final class Constant
+{
+    const XML = '<?xml version="1.0" encoding="UTF-8"?><foo><bar>beer</bar></foo>';
+    const TREE = [
+        'foo' => [
+            'bar' => 'beer',
+        ],
+    ];
+}
